fc8a33c06323f9f1810d58b4e731d2cd911d2a94
PhiltheHen <philip.d.henson@gmail.com>
2015-10-20 06:32:23 -0700

    Initial Commit

---
+++ TSCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TSCalculator
+//
+//  Created by Philip Henson on 10/20/15.
+//  Copyright © 2015 Phil Henson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
